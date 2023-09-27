13dda4c9cc856a23425c13467a09a37818b0633d
bsv-khiemtran <khiemtran@bravesoft.com.vn>
2023-09-27 16:03:12 +0700

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -11,48 +12,18 @@ import (
 	"github.com/inhies/go-bytesize"
 )
 
-var (
-	maxSize float64 = 100
-	header          = []string{
-		"key",
-		"input_channel_id",
-		"input_video_id",
-	}
-)
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("input file name: ")
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	fileName := strings.Replace(text, "\n", "", -1)
-	if fileName == "" {
-		log.Fatal("file name is empty")
-	}
+	maxSize := readFileSize(reader)
+	fileName := readFileName(reader)
 
-	fmt.Println("")
-	fmt.Println(`Valid byte units are "B", "KB", "MB", "GB", "TB", "PB" and "EB"]: `)
-	fmt.Print(`file size wanted: `)
-	text, err = reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	fileSizeWanted := strings.Replace(text, "\n", "", -1)
-	if fileName == "" {
-		log.Fatal("file size is empty")
-	}
-	b, err := bytesize.Parse(fileSizeWanted)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", b)
-	maxSize = float64(b)
+	header := readHeader(reader)
+	content := readContenFile(reader)
+	header = append(header, "created_at")
 
 	fmt.Println("begin gen file csv...")
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	file, err := os.Create(fmt.Sprintf("%v.csv", fileName))
 	if err != nil {
@@ -64,12 +35,8 @@ func main() {
 	fistRow := true
 	for {
 		writer := bufio.NewWriter(file)
-		rowContent := []string{
-			time.Now().UTC().String(),
-			"UCPW580o8ITQJFLCQlV6uuAw",
-			"LO3ZdbYK8yI",
-		}
 
+		rowContent := append(content, time.Now().UTC().Format(time.RFC3339Nano))
 		if fistRow {
 			rowContent = header
 			fistRow = false
@@ -92,4 +59,80 @@ func main() {
 
 	bt := bytesize.New(fileSize)
 	fmt.Printf("gen %s file [%s] csv success.\n", fileName, bt)
+}
+
+func readHeader(reader *bufio.Reader) (header []string) {
+	fmt.Print("input header csv [format with Comma]: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range strings.Split(text, ",") {
+		colName := strings.TrimSpace(v)
+		header = append(header, colName)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+	}
+	return header
+}
+
+func readFileName(reader *bufio.Reader) string {
+	fmt.Print("input file csv name (not included file extension): ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := strings.Replace(text, "\n", "", -1)
+	if fileName == "" {
+		log.Fatal("file name is empty")
+	}
+	return fileName
+}
+
+func readFileSize(reader *bufio.Reader) float64 {
+	fmt.Println(`Valid byte units are "B", "KB", "MB", "GB", "TB", "PB" and "EB"]: `)
+	fmt.Print(`file size wanted: `)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileSizeWanted := strings.Replace(text, "\n", "", -1)
+	b, err := bytesize.Parse(fileSizeWanted)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return float64(b)
+}
+
+func readContenFile(reader *bufio.Reader) []string {
+	fmt.Print(`content file [format with Comma]: `)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content := []string{}
+	for _, v := range strings.Split(text, ",") {
+		colName := strings.TrimSpace(v)
+		content = append(content, colName)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+	}
+
+	if len(content) == 0 {
+		log.Fatal("content file is empty")
+	}
+	return content
 }
